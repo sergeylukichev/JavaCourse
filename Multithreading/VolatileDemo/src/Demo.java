1d7658933472727a17558f6d2da3492bed5b35ec
Sergey Lukichev <s.lukichev@gmail.com>
2019-03-24 19:12:38 +0100

    volatile demo

---
+++ Multithreading/VolatileDemo/src/Demo.java
@@ -0,0 +1,36 @@
+import java.util.concurrent.TimeUnit;
+
+public class Demo {
+
+    private static boolean stopRequested = false;
+
+    private static synchronized boolean getRequestStatus () {
+        return stopRequested;
+    }
+
+    private static synchronized void stopRequested() {
+        stopRequested = true;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread backgroundThread = new Thread(new Runnable() {
+            public void run() {
+                int i = 0;
+                //while(!stopRequested){ <- if uncommented, will never stop
+                while (!getRequestStatus()) {
+                    i ++;
+                }
+                System.out.println(i);
+            }
+        });
+
+        backgroundThread.start();
+
+        TimeUnit.SECONDS.sleep(1);
+//        stopRequested = true; <-- if uncommented will never stop
+        stopRequested();
+    }
+}
+
+//while (!stopRequested) i++
+//turns into if(!stopRequested) while(true) i++
