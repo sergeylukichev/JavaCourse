0461a6edecc53984584b2a77ed48e76bd7835aa0
Sergey Lukichev <sergey.lukichev@zalando.de>
2020-03-03 21:23:54 +0100

    new setup for game

--- GuessNumber/src/Main.java
+++ GuessNumber/src/Main.java
@@ -1,10 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-        GuessNumberGame game = new GuessNumberGame(15,
-                3,
-                new KeyboardGuess(),
-                new ConsoleOutput());
+
+        KeyboardGuess source = new KeyboardGuess();
+        ConsoleOutput output = new ConsoleOutput();
+        int secretNumber = 15;
+        int attemptsCount = 3;
+
+        GuessNumberGame game = new GuessNumberGame(
+                secretNumber,
+                attemptsCount,
+                source,
+                output);
 
         game.play();
     }
