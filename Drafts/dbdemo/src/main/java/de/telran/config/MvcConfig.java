08425a7f338c84569490a4f8c10de5d752d4f443
Sergey Lukichev <sergey.lukichev@zalando.de>
2020-04-03 21:29:11 +0200

    security configurations

---
+++ Drafts/dbdemo/src/main/java/de/telran/config/MvcConfig.java
@@ -0,0 +1,16 @@
+package de.telran.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/students").setViewName("students");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
+
